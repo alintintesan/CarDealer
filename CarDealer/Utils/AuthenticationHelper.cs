d13797c205f5ffd04f6f1c27ecd349fd3df1cb81
ale.herm <ale.herm@yahoo.com>
2019-04-09 22:05:27 +0300

    added masked input for password in auth helper

---
+++ CarDealer/Utils/AuthenticationHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.Utils
+{
+    class AuthenticationHelper
+    {
+        public string ReadLineMasked(char mask = '*')
+        {
+            var password = new StringBuilder();
+            ConsoleKeyInfo keyInfo;
+            while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Enter)
+            {
+                if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    password.Append(keyInfo.KeyChar);
+                    Console.Write(mask);
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password.Remove(password.Length - 1, 1);
+
+                    if (Console.CursorLeft == 0)
+                    {
+                        Console.SetCursorPosition(Console.BufferWidth - 1, Console.CursorTop - 1);
+                        Console.Write(' ');
+                        Console.SetCursorPosition(Console.BufferWidth - 1, Console.CursorTop - 1);
+                    }
+                    else Console.Write("\b \b");
+                }
+            }
+            Console.WriteLine();
+            return password.ToString();
+        }
+    }
+}
