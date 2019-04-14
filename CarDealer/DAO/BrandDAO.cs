727e5f9ccee282526e058952c129c77394c49d11
alin.tintesan <tintesan_alin@yahoo.com>
2019-04-14 21:36:37 +0300

    added DAO for Brand

---
+++ CarDealer/DAO/BrandDAO.cs
@@ -0,0 +1,41 @@
+﻿using CarDealer.Interfaces;
+using CarDealer.Models;
+using CarDealer.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.DAO
+{
+    class BrandDAO : IBrandDAO
+    {
+        public List<Brand> GetAllBrands()
+        {
+            List<Brand> brands = new List<Brand>();
+
+            using (SqlConnection connection = DatabaseHelper.Instance.GetConnection())
+            {
+                connection.Open();
+                string sql = "select * from Brands";
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int id = (int)reader["id"];
+                    string brand = (string)reader["brand"];
+
+                    Brand brandObj = new Brand(id, brand);
+                    brands.Add(brandObj);
+                }
+                reader.Close();
+            }
+
+            return brands;
+        }
+    }
+}
